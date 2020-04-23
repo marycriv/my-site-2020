4ee680c40a0fcd6a6831541ce0e8e7659975bd51
marycriv <maryecrivelli@gmail.com>
2020-04-22 21:26:28 -0400

    about me component done

--- src/containers/AboutMeContainer.js
+++ src/containers/AboutMeContainer.js
@@ -3,8 +3,13 @@ import React from 'react';
 class AboutMeContainer extends React.Component {
     render(){
         return (
-            <div>
-                <p>AboutMeContainer</p>
+            <div className="about-me">
+                <center><h1>About Me</h1></center>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in sem eu nulla cursus fringilla nec eget orci. Sed bibendum bibendum tempus. Maecenas dignissim ullamcorper feugiat. Sed at lobortis massa. In massa purus, aliquet nec enim eu, tempus consectetur magna. Nam vulputate risus lacus, eget mollis dolor gravida eget. Phasellus ac bibendum quam. Suspendisse semper massa ac metus imperdiet, sit amet mollis purus sodales. Praesent bibendum tortor metus, eget dapibus ante tempor a.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in sem eu nulla cursus fringilla nec eget orci. Sed bibendum bibendum tempus. Maecenas dignissim ullamcorper feugiat. Sed at lobortis massa. In massa purus, aliquet nec enim eu, tempus consectetur magna. Nam vulputate risus lacus, eget mollis dolor gravida eget. Phasellus ac bibendum quam. Suspendisse semper massa ac metus imperdiet, sit amet mollis purus sodales. Praesent bibendum tortor metus, eget dapibus ante tempor a.</p>
+                
+                <img src="https://i.kym-cdn.com/entries/icons/facebook/000/021/807/ig9OoyenpxqdCQyABmOQBZDI0duHk2QZZmWg2Hxd4ro.jpg" alt="mr-robot-hackerman" width="100%" />
             </div>
         )
     }

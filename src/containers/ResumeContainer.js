82d870cccd349ed2ccb8632e487f4ec5bc1d38b9
marycriv <maryecrivelli@gmail.com>
2020-04-22 21:10:49 -0400

    education component added

--- src/containers/ResumeContainer.js
+++ src/containers/ResumeContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WorkProjectEntry from './../components/WorkProjectEntry';
+import EducationEntry from './../components/EducationEntry';
 
 class ResumeContainer extends React.Component {
     render(){
@@ -9,16 +10,22 @@ class ResumeContainer extends React.Component {
                     <center><h2>Title | Location | Resume PDF Link</h2></center>
                     <p>Personal Statement: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in sem eu nulla cursus fringilla nec eget orci. Sed bibendum bibendum tempus. Maecenas dignissim ullamcorper feugiat. Sed at lobortis massa. In massa purus, aliquet nec enim eu, tempus consectetur magna. Nam vulputate risus lacus, eget mollis dolor gravida eget. Phasellus ac bibendum quam. Suspendisse semper massa ac metus imperdiet, sit amet mollis purus sodales. Praesent bibendum tortor metus, eget dapibus ante tempor a.</p>
                 </div>
-                <hr/>
+                <hr />
                 <div className="work-experience">
                     <WorkProjectEntry />
                     <WorkProjectEntry />
                 </div>
+                <hr />
                 <div className="projects"> 
-
+                    <WorkProjectEntry />
+                    <WorkProjectEntry />
+                    <WorkProjectEntry />
+                    <WorkProjectEntry />
                 </div>
+                <hr />
                 <div className="education"> 
-
+                    <EducationEntry />
+                    <EducationEntry />
                 </div>
             </div>
         )

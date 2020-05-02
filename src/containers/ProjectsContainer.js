65a576bc659024f800b869a7fc1184dc49f91e12
marycriv <maryecrivelli@gmail.com>
2020-05-01 23:41:45 -0400

    projects container grid centered

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -4,16 +4,24 @@ import ProjectCard from './../components/ProjectCard';
 import BlogCard from './../components/BlogCard';
 import Grid from '@material-ui/core/Grid';
 
-
+const useStyles = makeStyles(theme => ({
+    gridCenter: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  }))
 
 export default function ProjectsContainer() {
 
     const [name] = useState("Project");
 
+    const classes = useStyles()
+
     return (
         <div className="projects">
             <center><h1>Projects</h1></center>
-            <Grid container spacing={3} flexGrow={1}>
+            <Grid container spacing={3} className={classes.gridCenter}>
                 <Grid item xs={3}>
                     <ProjectCard name={name} />
                 </Grid>
@@ -25,10 +33,16 @@ export default function ProjectsContainer() {
                 </Grid>
             </Grid>
             <center><h1>Blogs</h1></center>
-            <Grid container alignItems='center'>
-                <BlogCard />
-                <BlogCard />
-                <BlogCard />
+            <Grid container spacing={3} className={classes.gridCenter}>
+                <Grid item xs={3}>
+                    <BlogCard name={name} />
+                </Grid>
+                <Grid item xs={3}>
+                    <BlogCard name={name} />
+                </Grid>
+                <Grid item xs={3}>
+                    <BlogCard name={name} />
+                </Grid>
             </Grid>
         </div>
     )

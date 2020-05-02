49a447c335b3d50d6a3c77c947ab7732b0b405d1
marycriv <maryecrivelli@gmail.com>
2020-05-01 23:39:02 -0400

    projects grid initially stylized

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import ProjectCard from './../components/ProjectCard';
 import BlogCard from './../components/BlogCard';
 import Grid from '@material-ui/core/Grid';
-  
+
+
 
 export default function ProjectsContainer() {
+
     const [name] = useState("Project");
 
     return (
         <div className="projects">
             <center><h1>Projects</h1></center>
-            <Grid container alignItems='center'>
-                <ProjectCard name={name} />
-                <ProjectCard name={name} />
-                <ProjectCard name={name} />
+            <Grid container spacing={3} flexGrow={1}>
+                <Grid item xs={3}>
+                    <ProjectCard name={name} />
+                </Grid>
+                <Grid item xs={3}>
+                    <ProjectCard name={name} />
+                </Grid>
+                <Grid item xs={3}>
+                    <ProjectCard name={name} />
+                </Grid>
             </Grid>
             <center><h1>Blogs</h1></center>
             <Grid container alignItems='center'>
